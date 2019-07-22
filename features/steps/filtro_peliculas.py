6575a00a60c8a62f103cfafdc5ea3ee27de04b02
luis zuniga <lufezuro@gmail.com>
2019-07-21 23:54:37 -0500

    fix

--- features/steps/filtro_peliculas.py
+++ features/steps/filtro_peliculas.py
@@ -27,7 +27,7 @@ def step_impl(context, titulo):
 	context.titulo = titulo
 
 
-@when("busque la películas por título {criterio}")
+@when("busque la películas por {criterio}")
 def step_impl(context, criterio):
 	if(criterio == 'titulo'):
 		resultado, mensaje = get_pelicula_titulo(context.peliculas, context.titulo)
